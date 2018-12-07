b22292c2e064bd957e932b69574308420c4b6261
slimwizard <mrice@wireless-a-651.LaTech.edu>
2018-12-07 12:48:46 -0600

    first commit of angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) {}
   title = 'app';
+  url = '';
+  my_data: any
+
+
+  getNextMeme() {
+    this.url = this.my_data.data.children[Math.floor(Math.random() * this.my_data.data.children.length)].data.url
+  }
+
+  ngOnInit() {
+    this.http.get("https://www.reddit.com/r/programmerhumor/search.json?q=memes&limit=100")
+      .subscribe(data => { 
+        this.my_data = data
+        console.log(this.my_data)
+        this.url = this.my_data.data.children[Math.floor(Math.random() * this.my_data.data.children.length)].data.url
+        
+      })
+
+
+  }
+
+  
+
 }
